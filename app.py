2df96358c219d542fbc3adb0601f50663024106d
4915625 <arpithgmishra@gmail.com>
2026-04-29 13:54:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from datetime import datetime, date
 from flask import Flask,  flash, redirect, render_template, request, session
 from flask_session import Session
 from functools import wraps
+import os
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
